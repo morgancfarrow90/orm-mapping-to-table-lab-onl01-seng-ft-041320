83a492f7b5bbd74396ad26f9996ebb42ce8679d5
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-15 22:16:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,8 +40,13 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create 
-    
+  def self.create(name, grade)
+    def self.create(name, album)
+    song = Song.new(name, album)
+    song.save
+    song
+  end
+
   end
 
   
